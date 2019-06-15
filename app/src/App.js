afaf58a0aaa3880d017e08af1e94fe705c8fa8e9
wirasuta <wirasutat@gmail.com>
2019-06-15 11:07:59 +0700

    test lint

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
